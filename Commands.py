5081de1e21a52fbc4a9bc4c172b69c63709d8546
2233testr1122 <hashhhhashh104@gmail.com>
2024-05-21 16:54:37 +0200

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -18,16 +18,27 @@ from discord.ext import commands,tasks
 from PIL import Image, ImageDraw, ImageFont
 from pystyle import Colorate, Colors, Center, Box
 
+with open('selfbot.py', 'r', encoding='utf-8') as file:
+    code = file.read()
+
+def save_config(config_data):
+    with open('config.json', 'w') as file:
+        json.dump(config_data, file, indent=4)
+
+with open('config.json') as f:
+    config = json.load(f)
+    prefix = config['prefix']
+
 def setup(selfbot):
     @selfbot.command(name="search", description="Lets you search")
     async def search(ctx, *, query):
         query_string = urllib.parse.urlencode({'q': query})
         await ctx.send(f"[{query_string}](https://www.google.com/search?{query_string})")
-    
+        
     @selfbot.command()
     async def test(ctx):
         await ctx.send("sucessfull")
-    
+        
     @selfbot.command()
     async def prefix(new_prefix):
         with open('config.json', 'r') as f:
@@ -36,21 +47,3 @@ def setup(selfbot):
         with open('config.json', 'w') as f:
             json.dump(prefixes, f, indent=4)
         print(f"Prefix changed to: {new_prefix}")
-    
-    @selfbot.command()
-    async def copy(ctx, *urls):
-        await ctx.message.delete()
-        folder = "videos"
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        for url in urls:
-            try:
-                youtube = YouTube(url)
-                if youtube.length and youtube.description and youtube.length < 60 and youtube.description.startswith("Shorts"):
-                    video = youtube.streams.filter(only_video=True).order_by('resolution').desc().first()
-                else:
-                    video = youtube.streams.get_highest_resolution()
-                video.download(output_path=folder)
-                print(tk.END, f"{SUCCESS} Video copyied: {url}")
-            except Exception as e:
-                print(f"{ERROR} {str(e)}")
