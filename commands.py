d3f784ec77c7dc985c04dfb60efb553a93a2dfa3
2233testr1122 <hashhhhashh104@gmail.com>
2024-07-17 00:22:05 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,5 +1,8 @@
+# You can only make commands in this file
+# Use any code editor for this such as VSCode, etc
+
 from discord.ext import commands
 import discord
 
-async def test(ctx):
-    await ctx.send("test!")
+async def example(ctx):
+    await ctx.send("its working, wow!")
